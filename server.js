91e1153315da1dfe93841f492faac5c0b881eb97
Kesavan Rangan <M1039737@mindtree.com>
2019-12-23 23:30:09 +0530

    Testing the pipeline - 2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {    
-	res.send('Hello world from a Node.js appp- kesavan! This comes from an automated pipeline')
+	res.send('<html><br>Hello world from a Node.js appp- kesavan!<br><br> This comes from an automated pipeline</html>')
 })
 app.listen(3000, () => {
 	console.log('Server is up on 3000')})
