11927b35c4f1aca477f3ba68bbc862a0b7ec4aa6
Kesavan Rangan <M1039737@mindtree.com>
2019-12-23 21:48:10 +0530

    Checking the pipeling with an edit in the present config

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {    
-	res.send('Hello world from a Node.js appp- kesavan!')
+	res.send('Hello world from a Node.js appp- kesavan! This comes from an automated pipeline')
 })
 app.listen(3000, () => {
 	console.log('Server is up on 3000')})
