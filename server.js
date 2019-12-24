77a3f9e98e1cacbc1e1961efb9b20bf7fc1aa0f3
Kesavan Rangan <M1039737@mindtree.com>
2019-12-24 20:06:25 +0530

    Edit

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {    
-	//res.send('<html><br>Hello world from a Node.js appp- kesavan!<br><br> This comes from an automated pipeline</html>')
+	res.send('<html><br>Hello world from a Node.js appp- kesavan!<br><br> This comes from an automated pipeline</html>')
 	//res.send('<html><br><p align="center"> HEY THERE!</p></html>')
-	res.sendFile('/home/Node-Web-App/templates/body.html')
+	//res.sendFile('/home/Node-Web-App/templates/body.html')
 })
 app.listen(3000, () => {
 	console.log('Server is up on 3000')})
