1b5447e0490c26ca362c4a51a74c1480da55cf46
Kesavan Rangan <M1039737@mindtree.com>
2019-12-24 18:17:12 +0530

    An update to the html file

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {    
-	res.send('<html><br>Hello world from a Node.js appp- kesavan!<br><br> This comes from an automated pipeline</html>')
+	//res.send('<html><br>Hello world from a Node.js appp- kesavan!<br><br> This comes from an automated pipeline</html>')
 	//res.send('<html><br><p align="center"> HEY THERE!</p></html>')
+	res.sendFile('C:/Users/m1039737/Desktop/Project/afaria_data/body.html')
 })
 app.listen(3000, () => {
 	console.log('Server is up on 3000')})
