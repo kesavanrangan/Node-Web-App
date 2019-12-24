a42f1afdee8971c41660d9e01e5ea4e942a71db2
Kesavan Rangan <M1039737@mindtree.com>
2019-12-24 17:32:47 +0530

    A change in the Front-end file

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {    
-	//res.send('<html><br>Hello world from a Node.js appp- kesavan!<br><br> This comes from an automated pipeline</html>')
-	res.send('<html><br><p align="center"> HEY THERE!</p></html>')
+	res.send('<html><br>Hello world from a Node.js appp- kesavan!<br><br> This comes from an automated pipeline</html>')
+	//res.send('<html><br><p align="center"> HEY THERE!</p></html>')
 })
 app.listen(3000, () => {
 	console.log('Server is up on 3000')})
