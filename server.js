3dda4d73f38ca5c06bfbc93de4da7410ee475a1d
Kesavan Rangan <kesavanrangan@gmail.com>
2019-12-25 19:37:48 +0530

    Templates are added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 app.get('/', (req, res) => {    
 	//res.send('<html><br>Hello world from a Node.js appp- kesavan!<br><br> This comes from an automated pipeline</html>')
 	//res.send('<html><br><p align="center"> HEY THERE!</p></html>')
-	res.sendFile('/usr/src/app/templates/body.html')
+	res.sendFile('/usr/src/app/templates/index.html')
 })
 app.listen(3000, () => {
 	console.log('Server is up on 3000')})
