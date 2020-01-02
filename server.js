69b3cb03f23e84eaeccba1aeae252baea502f9da
Kesavan Rangan <kesavanrangan@gmail.com>
2020-01-02 20:37:12 +0530

    Final Update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {    
-	res.send('<html><title>Node Web App</title><h1>Hello world from a Node.js app !!!</h1><p><h4> This comes from an automated pipeline created by Kesavan Rangan using AWS PipeLine</h4></p><br><br>Thanks</html>')
+	res.send('<html><title>Node Web App</title><h1>Hello world from a Node.js app !!!</h1><p><h4> This comes from an automated pipeline created by Kesavan Rangan using AWS PipeLine</h4></p><br>Thanks</html>')
 })
 app.listen(3000, () => {
 	console.log('Server is up on 3000')})
