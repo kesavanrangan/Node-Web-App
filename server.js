f209ba577a712f41417348fa86703c47caa5d71b
Kesavan Rangan <kesavanrangan@gmail.com>
2020-01-02 19:37:37 +0530

    A new update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {    
-	res.send('<html><br>Hello world from a Node.js appp- kesavan!<br><br> This comes from an automated pipeline</html>')
+	res.send('<html><br>Hello world from a Node.js appp- kesavan!<br><br> This comes from an automated pipeline<p>A NEW UPDATE!!!</p></html>')
 	//res.sendFile('/usr/src/app/templates/index.html')
 })
 app.listen(3000, () => {
