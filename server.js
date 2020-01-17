4152109a65941d826473e37201a6f9cb59209b29
Kesavan Rangan <kesavanrangan@gmail.com>
2020-01-18 01:24:39 +0530

    Another commit to the file

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = express()
 app.get('/', (req, res) => {    
 	res.send('<html><title>Node Web App FROM JENKINS</title><body><font face="Courier New" color="Red"><h1>Hello world from a <b>Node.js app!!!</b></h1><p><h4> This comes from an automated pipeline created by <i>Kesavan Rangan</i> using <b>JENKINS PIPELINE</b>.</h4></p><br>Thanks</font></body></html>')
 })
-app.listen(3000, () => {
+app.listen(80, () => {
 	console.log('Server is up on 3000')})
