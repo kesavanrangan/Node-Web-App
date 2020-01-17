f7dcc68466f9f760d304b083ec715c09d2d23697
Kesavan Rangan <kesavanrangan@gmail.com>
2020-01-18 01:12:11 +0530

    A change in the server.js file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {    
-	res.send('<html><title>Node Web App FROM JENKINS</title><body><font face="Courier New" color="black"><h1>Hello world from a <b>Node.js app!!!</b></h1><p><h4> This comes from an automated pipeline created by <i>Kesavan Rangan</i> using <b>JENKINS PIPELINE</b>.</h4></p><br>Thanks</font></body></html>')
+	res.send('<html><title>Node Web App FROM JENKINS</title><body><font face="Courier New" color="Red"><h1>Hello world from a <b>Node.js app!!!</b></h1><p><h4> This comes from an automated pipeline created by <i>Kesavan Rangan</i> using <b>JENKINS PIPELINE</b>.</h4></p><br>Thanks</font></body></html>')
 })
 app.listen(3000, () => {
 	console.log('Server is up on 3000')})
